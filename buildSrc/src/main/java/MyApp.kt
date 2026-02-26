28c84e8494993ae3013df37fda6fba144ba40b13
Rynntan <rynntan0@gmail.com>
2026-02-26 15:22:18 +0800

    Try a new template

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "io.rynntan.pyroscape"
-    const val version = 6
-    const val versionName = "0.1.5"
+    const val version = 7
+    const val versionName = "0.1.6"
 }
