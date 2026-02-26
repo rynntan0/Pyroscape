beaab5ee74a69b4e038a4d6abf83f7fdc376a8ac
Rynntan <rynntan0@gmail.com>
2026-02-26 14:46:05 +0800

    Fix xml icon template

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "io.rynntan.pyroscape"
-    const val version = 5
-    const val versionName = "0.1.4"
+    const val version = 6
+    const val versionName = "0.1.5"
 }
