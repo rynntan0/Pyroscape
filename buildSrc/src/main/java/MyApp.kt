a9b2a5498158ee0a65e11d6fa5be276d0a732583
Rynntan <rynntan0@gmail.com>
2026-05-11 01:28:31 +0800

    Bug fix.

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "io.rynntan.pyroscape"
-    const val version = 9
-    const val versionName = "0.1.8"
+    const val version = 10
+    const val versionName = "0.1.9"
 }
